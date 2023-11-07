aa5b7ada123e01b4e87ba3c9cdc14501fbddf81b
Tiaguh <tiagosingup@gmail.com>
2023-11-06 23:05:04 -0300

    first commit

---
+++ Learning C#/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Learning_C_
+{	
+	public partial class MainForm : Form
+	{
+		int numeroConta;	
+		double saldoConta = 100.0;
+		double valorSaque = 10.0;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			numeroConta = 1;
+			saldoConta -= valorSaque;
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Conta: " + numeroConta + "\nSaldo: " + saldoConta);
+		}
+	}
+}
