841e499ec9dbe3c5c4953cc7a33d59d75240f877
Tiaguh <tiagosingup@gmail.com>
2023-11-07 22:06:09 -0300

    making a exercise

--- Learning C#/MainForm.cs
+++ Learning C#/MainForm.cs
@@ -4,24 +4,36 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace Learning_C_
-{	
-	public partial class MainForm : Form
-	{
-		int numeroConta;	
-		double saldoConta = 100.0;
-		double valorSaque = 10.0;
-		
-		public MainForm()
-		{
-			InitializeComponent();
-			
-			numeroConta = 1;
-			saldoConta -= valorSaque;
-		}
-		
-		void Button1Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("Conta: " + numeroConta + "\nSaldo: " + saldoConta);
-		}
-	}
+{   
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();                    
+        }
+        
+        void Button1Click(object sender, EventArgs e)
+        {   
+            int a = 2;
+            int b = 3;
+            int c = 4;
+
+            double delta, a1, a2;
+
+            delta = b * b - 4 * a * c;
+
+            if (delta >= 0)
+            {
+                a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                MessageBox.Show(a1.ToString()); // Exibe a primeira raiz se delta for não negativo
+                MessageBox.Show(a2.ToString()); // Exibe a segunda raiz se delta for não negativo
+            }
+            else
+            {
+                MessageBox.Show("Não há raízes reais para a equação.");
+            }
+        }
+    }
 }
